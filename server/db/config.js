4e1f2b024d93a04eb80d5a5c15e4873007264055
Spifet <jei.cohen@gmail.com>
2022-06-20 23:11:55 +0300

    fixing client branch

--- server/db/config.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-async function initDbConnection() {
-  await mongoose.connect(
-    process.env.MONGO_URI || 'mongodb://localhost:27017/end-project'
-  );
-}
-
-export default initDbConnection;
